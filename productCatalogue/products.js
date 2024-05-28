3e5dcf8607a988ef78333cd9afe41e62e0241984
Paul1ofph <pauladibe24@gmail.com>
2024-05-28 10:10:42 +0100

    updated

--- productCatalogue/products.js
+++ productCatalogue/products.js
@@ -31,6 +31,15 @@ activePanel.addEventListener(
 );
 });
 
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
 
 const boxes = document.querySelectorAll(".box");
 const triggerBottom = window.innerHeight / 2 * 5;
